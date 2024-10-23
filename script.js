3e32878837fbbd752beb0caf8c0703b6fb89071b
jiths3 <jithinchacko71@gmail.com>
2024-10-23 23:36:05 +0530

    array storage

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     calendar.render(); // Render the calendar
 });
+
+
+// storing the habits 
+
+var habitsarray = [];
+
+
+document.addEventListener('DOMContentLoaded', function() {
+var habitsarray = [];
+   
+   
+let habitform = document.getElementById("habit-form");
+
+
+habitform.addEventListener('submit', function(event){
+
+event.preventDefault();
+
+let habit1 = document.getElementById("habit1").value; 
+let habit2 = document.getElementById("habit2").value; 
+let habit3 = document.getElementById("habit3").value;
+let habit4 = document.getElementById("habit4").value; 
+let habit5 = document.getElementById("habit5").value; 
+
+habitsarray.push(habit1);
+habitsarray.push(habit2);
+habitsarray.push(habit3);
+habitsarray.push(habit4);
+habitsarray.push(habit5);
+
+console.log(habitsarray);
+
+}
+
+
+
+)
+});
