f767de83a1e1b26c41e7f66ff99784e8cc40d1e1
jiths3 <jithinchacko71@gmail.com>
2024-11-15 22:32:05 +0530

    simple navbar

--- script2.js
+++ script2.js
@@ -3,6 +3,7 @@ firsthabit.innerHTML = habitsunstring[0];
 secondhabit.innerHTML = habitsunstring[1];
 thirdhabit.innerHTML = habitsunstring[2];
 fourthhabit.innerHTML = habitsunstring[3];
+
 //ezzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
 
 
